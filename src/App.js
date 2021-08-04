6577a116892d29aaafcadfc660a136544d612fe3
PavloYurchyshynn <gamepaha123@gmail.com>
2021-08-04 16:02:45 +0300

    release v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import ToDo from './ToDo'
+import ToDoForm from './ToDoForm'
+
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+
+  const addTask = () => {
+
+  }
+
+  const removeTask = () => {
+
+  }
+
+  const handleToggle = () => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Список завдань: {todos.length}</h1>
       </header>
+      <ToDoForm />
+      {todos.map((todo) => {
+        return (
+          <ToDo key={todo.id}  />
+        )
+      })}
     </div>
   );
 }
