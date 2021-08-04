74e32ca5980a5950bc28ee2f4d3a6e2c2393a6c7
PavloYurchyshynn <gamepaha123@gmail.com>
2021-08-04 17:43:13 +0300

    release v2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ function App() {
 
   const [todos, setTodos] = useState([])
 
-  const addTask = () => {
-
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substr(2, 9),
+        task: userInput,
+        complete: false
+      }
+      setTodos([...todos, newItem])
+    }
   }
 
   const removeTask = () => {
@@ -24,10 +31,10 @@ function App() {
       <header>
         <h1>Список завдань: {todos.length}</h1>
       </header>
-      <ToDoForm />
+      <ToDoForm addTask={addTask}/>
       {todos.map((todo) => {
         return (
-          <ToDo key={todo.id}  />
+          <ToDo key={todo.id} todo={todo} toggleTask={handleToggle} removeTask={removeTask}/>
         )
       })}
     </div>
--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,5 +1,33 @@
-function ToDoForm() {
-    return null
+import { useState } from 'react'
+
+function ToDoForm({ addTask }) {
+    const [userInput, setUserInput] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addTask(userInput)
+    }
+
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value)
+    }
+
+    const handleKeyPress = () => {
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                value={userInput}
+                type='text'
+                onChange={handleChange}
+                onKeyDown={handleKeyPress}
+                placeholder='Введіть значення'
+            />
+            <button>Зберегти</button>
+        </form>
+    )
 }
 
 export default ToDoForm
